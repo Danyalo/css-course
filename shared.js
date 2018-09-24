98bcb8fed3e85e1fa7c89464a55d3f82426a8e08
Danylo Labenskyi <danylolab@gmail.com>
2018-09-24 10:18:01 +0300

    Finish Lesson 13

--- shared.js
+++ shared.js
@@ -1,18 +1,25 @@
-let backdrop = document.querySelector(".backdrop");
-let modal = document.querySelector(".modal");
-let modalNoButton = document.querySelector(".modal__action--negative");
-let selectPlanButtons = document.querySelectorAll(".plan button");
-let toggleButton = document.querySelector(".toggle-button");
-let mobileNav = document.querySelector(".mobile-nav");
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
+// console.dir(backdrop.style['background-image']);
+
+// console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function() {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open'; // This will actually overwrite the complete class list
     modal.classList.add("open");
     backdrop.classList.add("open");
   });
 }
 
 backdrop.addEventListener("click", function() {
+  // mobileNav.style.display = 'none';
   mobileNav.classList.remove("open");
   closeModal();
 });
@@ -22,6 +29,8 @@ if (modalNoButton) {
 }
 
 function closeModal() {
+  //   backdrop.style.display = "none";
+  //   modal.style.display = "none";
   if (modal) {
     modal.classList.remove("open");
   }
@@ -29,6 +38,8 @@ function closeModal() {
 }
 
 toggleButton.addEventListener("click", function() {
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
   mobileNav.classList.add("open");
   backdrop.classList.add("open");
 });
